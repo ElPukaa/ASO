af2b8888a94308aad8e5a938c561597d1be9cf05
HCalipo <h.polomolina@um.es>
2025-10-28 11:51:57 +0100

    REVISAR LO PREGUNTADO POR CHATI

--- syscals/TAREA/exec_lines.c
+++ syscals/TAREA/exec_lines.c
@@ -7,6 +7,8 @@
 #include <sys/wait.h>
 #include <errno.h>
 #include <fcntl.h>  
+#include <signal.h>
+#include <string.h> // Para memset
 
 int PROCS_ACTIVOS = 0; //contador de procesos activos
 int NUM_PROCS = 1;     //número máximo de procesos simultáneos(POR DEFECTO 1)
@@ -20,6 +22,43 @@ const char *mensaje_help =
     "-b BUF_SIZE \t Tamaño del buffer de entrada 1<=BUF_SIZE<=8192\n"
     "-l MAX_LINE_SIZE \t Tamaño máximo de línea 16<=MAX_LINE_SIZE<=1024\n"
     "-p NUM_PROCS \t Número de procesos en ejecución de forma simultánea (1 <= NUM_PROCS <= 8)\n";
+
+void instala_manejador_signal(int signal, void (*signal_handler)(int)) {
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(struct sigaction));
+    sa.sa_handler = signal_handler;
+    sa.sa_flags = SA_NOCLDSTOP; // Importante para SIGCHLD
+    sigemptyset(&sa.sa_mask);
+    if (sigaction(signal, &sa, NULL) == -1) {
+        perror("sigaction()");
+        exit(EXIT_FAILURE);
+    }
+}
+
+
+void manejador_sigchld(int signal) {
+    int saved_errno = errno;
+    pid_t pid;
+    int status;
+
+    // Bucle para cosechar a TODOS los hijos que hayan terminado
+    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {     // Mientras haya hijos que hayan terminado
+        PROCS_ACTIVOS--; // Decrementa el contador por cada hijo terminado
+
+        int status;                             //para almacenar el estado de terminación
+
+        if (pid > 0) {//si waitpid ha ido bien (devuelve -1 en caso de error sino el pid del proceso que ha terminado)   
+            if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {//si el proceso ha terminado con exit pero con código de error distinto de 0
+                fprintf(stderr, "Error al ejecutar la línea %d. Terminación normal con código %d.\n", LINENO, WEXITSTATUS(status));
+                
+            } else if (WIFSIGNALED(status)) {//si el proceso ha terminado por señal de sistema(forzado)
+                fprintf(stderr, "Error al ejecutar la línea %d. Terminación anormal por señal %d.\n", LINENO, WTERMSIG(status));
+                
+            }
+        }
+    }
+    errno = saved_errno;
+}
 
 
 char* limpiar_linea(char* linea) {
@@ -66,9 +105,9 @@ void ejecutar_comando(char *comando) {
     exit(EXIT_FAILURE);
 }
 
-void esperar_hueco(int procesos_necesarios){
+/*void esperar_hueco(int procesos_necesarios){
 
-    if(PROCS_ACTIVOS + procesos_necesarios >= NUM_PROCS) { //mientras NO haya hueco para ejecutar otro proceso
+    if(PROCS_ACTIVOS >= NUM_PROCS) { //mientras NO haya hueco para ejecutar otro proceso
         int status;                             //para almacenar el estado de terminación
         pid_t terminado = wait(&status);        //espera a que termine un proceso hijo
         PROCS_ACTIVOS--;                        //decrementa el contador de procesos activos (porque despues del wait ha terminado uno)
@@ -83,81 +122,131 @@ void esperar_hueco(int procesos_necesarios){
             }
         }
     }
-}
+}*/
 
 
 void interpretar_comando(char *comando, int LINENO) {
     pid_t pid;              //para el fork
-    char *comando2 = NULL;
-    int pipefd[2];          //para las tuberias
-    int num_procs_necesarios = 1;
+    char *derecha = NULL;
+    char *izquierda = NULL; 
 
+    sigset_t blocked_signals, old_signals;
+    sigemptyset(&blocked_signals);
+    sigaddset(&blocked_signals, SIGCHLD);
+
+    // Bloquea SIGCHLD para comprobar PROCS_ACTIVOS de forma segura
+    if (sigprocmask(SIG_BLOCK, &blocked_signals, &old_signals) == -1) {
+        perror("sigprocmask(BLOCK)");
+        exit(EXIT_FAILURE);
+    }
+    
     //CUIDAO que pasa si no se lanzan tantos procesos como NUM_PROCS_GLOBAL? no entraria nunca aqui
     
-    esperar_hueco(num_procs_necesarios); //espera hasta que haya 2 huecos para lanzar otro proceso(en caso de que se haya llegado al maximo sino salta directamente)
+    
+    if ((derecha = strstr(comando, "|")) != NULL) {//si hay una tuberia
+        
+        int pipefd[2];          //para las tuberias
 
-    if ((comando2 = strstr(comando, "|")) != NULL) {//si hay una tuberia
-        *comando2 = '\0';   //cambia el carácter | por \0 para indicar el final del primer comando
-        comando2++;         //avanzar uno para que apunte al inicio del segundo comando
-        comando = limpiar_linea(comando);
-        comando2 = limpiar_linea(comando2);
-
+        *derecha = '\0';   //cambia el carácter | por \0 para indicar el final del primer comando
+        derecha++;         //avanzar uno para que apunte al inicio del segundo comando
+        izquierda = limpiar_linea(comando);
+        derecha = limpiar_linea(derecha);
+        
         //if(PROCS_ACTIVOS + 2 = NUM_PROCS_GLOBAL)
             //como ya hay un hueco reservado al menos, hacemos una segunda comprobación para tener seguro que hay al menos 2 huecos si hay pipeline 
     
+            if (pipe(pipefds) == -1){ /* Paso 0: Creación de la tubería */
+            
+                perror("pipe()");
+                exit(EXIT_FAILURE);
+            }
+            
+            /* Paso 1: Creación del hijo izquierdo de la tubería */
             pid = fork();
-            switch (pid) {
-                case -1:   //fallo del fork
-                    perror("fork");
+            switch (pid){
+                case -1:
+                    perror("fork(1)");
                     exit(EXIT_FAILURE);
                     break;
-
-                case 0: {   
-                    if (pipe(pipefd) == -1) {   //fallo al crear la tuberia
-                        perror("pipe");
+                    
+                case 0: /* Hijo izquierdo de la tubería */
+                    /* Paso 2: El extremo de lectura no se usa */
+                    if (close(pipefds[0]) == -1){
+                        perror("close(1)");
                         exit(EXIT_FAILURE);
                     }
-                    
-                    
-                    pid_t left = fork();    //parte izquierda de la tuberia
-                    if (left == 0) {
-                        close(pipefd[0]);   //cierra el extremo de lectura
-                        dup2(pipefd[1], STDOUT_FILENO); //redirige la salida estandar a la escritura de la tuberia
-                        close(pipefd[1]);   //cierra el descriptor ORIGINAL de escritura
-
-                        ejecutar_comando(comando);
-                          
+                    /* Paso 3: Redirige la salida estándar al extremo de escritura de la tubería */
+                    if (dup2(pipefds[1], STDOUT_FILENO) == -1){
+                        perror("dup2(1)");
+                        exit(EXIT_FAILURE);
                     }
-                    
-                    pid_t right = fork();   //parte derecha de la tuberia
-                    if (right == 0) {
-                        close(pipefd[1]);   //cierra el extremo de escritura
-                        dup2(pipefd[0], STDIN_FILENO);  
-                        close(pipefd[0]);   //crierra el descriptor ORIGINAL de lectura
-                        ejecutar_comando(comando2);
-                        
+                    /* Paso 4: Cierra el descriptor duplicado */
+                    if (close(pipefds[1]) == -1){
+                        perror("close(2)");
+                        exit(EXIT_FAILURE);
                     }
-                    //una vez lanzados los procesos, cierra las tuberias el padre y espera a que los hisjos terminen
-                    close(pipefd[0]);
-                    close(pipefd[1]);
-                    
-                    waitpid(left, NULL, 0);
-                    waitpid(right, NULL, 0);
-
-                    exit(EXIT_SUCCESS);
-                    }
+                    /* Paso 5: Reemplaza el binario actual por el de `youtube-dl` */
+                    ejecutar_comando(izquierda);
+                    perror("execlp(izquierdo)");
+                    exit(EXIT_FAILURE);
                     break;
-
-                default://no debería llegar el hijo aquí nunca, solo el padre
-                    PROCS_ACTIVOS++;
+                default: /* El proceso padre continúa... */
                     break;
             }
-        //}
-    }else if ((comando2 = strstr(comando, "<")) != NULL) {
-        *comando2 = '\0';
-        comando2++;
-        comando = limpiar_linea(comando);
-        comando2 = limpiar_linea(comando2);
+
+            /* Paso 6: Creación del hijo derecho de la tubería */
+            switch (fork()){
+
+                case -1:
+                    perror("fork(2)");
+                    exit(EXIT_FAILURE);
+                    break;
+                case 0: /* Hijo derecho de la tubería  */
+                    /* Paso 7: El extremo de escritura no se usa */
+                    if (close(pipefds[1]) == -1)
+                    {
+                        perror("close(3)");
+                        exit(EXIT_FAILURE);
+                    }
+                    /* Paso 8: Redirige la entrada estándar al extremo de lectura de la tubería */
+                    if (dup2(pipefds[0], STDIN_FILENO) == -1)
+                    {
+                        perror("dup2(2)");
+                        exit(EXIT_FAILURE);
+                    }
+                    /* Paso 9: Cierra el descriptor duplicado */
+                    if (close(pipefds[0]) == -1)
+                    {
+                        perror("close(4)");
+                        exit(EXIT_FAILURE);
+                    }
+                    /* Paso 10: Reemplaza el binario actual por el de `ffmpeg` */
+                    
+                    ejecutar_comando(derecha);
+                    perror("execlp(derecho)");
+                    exit(EXIT_FAILURE);
+                    break;
+                    
+                default: /* El proceso padre continúa... */
+                    break;
+            }
+            PROCS_ACTIVOS++; //NO SE SI METERLO AQUI O DONDE, PREGUNTAR A CHATI
+
+            /* El proceso padre cierra los descriptores de fichero no usados */
+            if (close(pipefds[0]) == -1){
+                perror("close(pipefds[0])");
+                exit(EXIT_FAILURE);
+            }
+            if (close(pipefds[1]) == -1){
+                perror("close(pipefds[1])");
+                exit(EXIT_FAILURE);
+            }
+            
+    }else if ((derecha = strstr(comando, "<")) != NULL) {
+        *derecha = '\0';
+        derecha++;
+        izquierda = limpiar_linea(comando);
+        derecha = limpiar_linea(derecha);
 
         
         pid = fork();
@@ -168,7 +257,7 @@ void interpretar_comando(char *comando, int LINENO) {
                 break;
 
             case 0: {   //si va bien
-                int fd = open(comando2, O_RDONLY);
+                int fd = open(derecha, O_RDONLY);
                 if (fd < 0) {
                     perror("open <");
                     exit(EXIT_FAILURE);
@@ -176,7 +265,7 @@ void interpretar_comando(char *comando, int LINENO) {
 
                 dup2(fd, STDIN_FILENO); //redirige la entrada estandar al fichero
                 close(fd);      //cierra el descriptor original
-                ejecutar_comando(comando);
+                ejecutar_comando(izquierda);
             }
                 break;
 
@@ -185,11 +274,11 @@ void interpretar_comando(char *comando, int LINENO) {
                 break;
         }
 
-    }else if ((comando2 = strstr(comando, ">>")) != NULL) {
-        *comando2 = '\0';
-        comando2 += 2;  //avanza dos posiciones para que apunte al inicio del segundo comando(por el >>)
-        comando = limpiar_linea(comando);
-        comando2 = limpiar_linea(comando2);
+    }else if ((derecha = strstr(comando, ">>")) != NULL) {
+        *derecha = '\0';
+        derecha += 2;  //avanza dos posiciones para que apunte al inicio del segundo comando(por el >>)
+        izquierda = limpiar_linea(comando);
+        derecha = limpiar_linea(derecha);
         
         pid = fork();
         switch (pid) {
@@ -199,7 +288,7 @@ void interpretar_comando(char *comando, int LINENO) {
                 break;
 
             case 0: {
-                int fd = open(comando2, O_WRONLY | O_CREAT | O_APPEND, S_IRWXU | S_IRWXG | S_IRWXO );//abre el fichero en modo escritura, si no existe lo crea y 
+                int fd = open(derecha, O_WRONLY | O_CREAT | O_APPEND, S_IRWXU | S_IRWXG | S_IRWXO );//abre el fichero en modo escritura, si no existe lo crea y 
                 // añade al final(permisos de  r w x para User, Group y Others la ultima letra los identifica)
                 if (fd < 0) {   //si hay error al abrir el fichero
                     perror("open >>");
@@ -207,7 +296,7 @@ void interpretar_comando(char *comando, int LINENO) {
                 }
                 dup2(fd, STDOUT_FILENO);//redirige la salida estandar al fichero
                 close(fd);//cierra el descriptor original
-                ejecutar_comando(comando);
+                ejecutar_comando(izquierda);
                 
                 }
                 break;
@@ -217,11 +306,11 @@ void interpretar_comando(char *comando, int LINENO) {
                 break;
         }
         
-    }else if ((comando2 = strstr(comando, ">")) != NULL) {
-        *comando2 = '\0';
-        comando2++;
-        comando = limpiar_linea(comando);
-        comando2 = limpiar_linea(comando2);
+    }else if ((derecha = strstr(comando, ">")) != NULL) {
+        *derecha = '\0';
+        derecha++;
+        izquierda = limpiar_linea(comando);
+        derecha = limpiar_linea(derecha);
 
         
         pid = fork();
@@ -232,7 +321,7 @@ void interpretar_comando(char *comando, int LINENO) {
                 break;
 
             case 0: {
-                int fd = open(comando2, O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU | S_IRWXG | S_IRWXO );//abre el fichero en modo escritura, 
+                int fd = open(derecha, O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU | S_IRWXG | S_IRWXO );//abre el fichero en modo escritura, 
                 // si no existe lo crea y si existe lo trunca
                 if (fd < 0) {   //si hay error al abrir el fichero
                     perror("open >");
@@ -241,7 +330,7 @@ void interpretar_comando(char *comando, int LINENO) {
 
                 dup2(fd, STDOUT_FILENO);    //redirige la salida estandar al fichero
                 close(fd);
-                ejecutar_comando(comando);
+                ejecutar_comando(izquierda);
             
                 }
                 break;
@@ -273,6 +362,7 @@ void interpretar_comando(char *comando, int LINENO) {
 
 
 int main(int argc, char *argv[]) {
+    instala_manejador_signal(SIGCHLD, manejador_sigchld);
     int opt, BUF_SIZE = 16, MAX_LINE_SIZE = 32; //valores por defecto 
     char *b_buffer; // buffer de lectura
     
@@ -381,11 +471,28 @@ int main(int argc, char *argv[]) {
         l_buffer[indice_bytes_lineas] = '\0';
         interpretar_comando(l_buffer, LINENO);
     }
+
+    // espera al final a que todos los hijos terminen antes de acabar con el programa para no dejar huerfanos
+    sigset_t blocked_signals, old_signals;
+    sigemptyset(&blocked_signals);
+    sigaddset(&blocked_signals, SIGCHLD);
+    sigprocmask(SIG_BLOCK, &blocked_signals, &old_signals);
+
+    while (PROCS_ACTIVOS > 0) {//mientras hay procesos activos
+        sigsuspend(&old_signals);
+    }
+
+    sigprocmask(SIG_SETMASK, &old_signals, NULL); // Restaurar máscara
+
     
-    free(l_buffer);
+    free(l_buffer);//liberar la memoria de bufe
     free(b_buffer);
     return 0;
 }
+
+
+
+
 
 
 
